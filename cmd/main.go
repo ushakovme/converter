fd7e9a68f8fc68d8795dc509266d451d4d718306
Ilya Ushakov <ilya@ushakov.me>
2021-06-12 18:13:11 +0300

    add graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/ushakovme/converter/pkg/converter/infrastructure"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 const httpAddr = "127.0.0.1:8000"
 
 func main() {
 	fmt.Println("starting converter")
-	err := do()
-	if err != nil {
-		panic(err)
+	ctx := getSignalCancelContext()
+
+	if err := serve(ctx); err != nil {
+		fmt.Println("converter stopped with error: ")
+		fmt.Println(err)
+		os.Exit(1)
 	}
+
+	fmt.Println("")
 	fmt.Println("converter stopped")
 }
 
-func do() error {
+func getSignalCancelContext() context.Context {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	go func() {
+		<-sigs
+		cancelFunc()
+	}()
+	return ctx
+}
+
+func serve(ctx context.Context) error {
 	converter := infrastructure.NewConverter()
 
 	mux := http.NewServeMux()
@@ -40,5 +59,21 @@ func do() error {
 		}
 		err = converter.PNGToJPG(pngFile, jpgFile)
 	})
-	return http.ListenAndServe(httpAddr, mux)
+	server := http.Server{
+		Addr: httpAddr,
+	}
+
+	server.Handler = mux
+	go func() {
+		<-ctx.Done()
+		if err := server.Shutdown(context.Background()); err != nil {
+			fmt.Println("HTTP server ListenAndServe: ", err)
+		}
+	}()
+
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		return err
+	}
+
+	return nil
 }
